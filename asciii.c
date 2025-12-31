b13ce5684fcf9b7f562015ce5d7b59037495f888
aakki3521-afk <aakki3521@gmail.com>
2025-12-31 18:03:22 +0000

    Add files via upload

---
+++ asciii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char n,i;
+    scanf("%c",&n);
+    for(i=65;i<=n;i++)
+    {
+        printf("%d\n%c\n",i,i );
+    }
+    
+    
+
+    
+    
+    return 0;
+}
---
+++ star.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i,b;
+for(i=1;i<=5;i++){
+    for(b=1;b<=i;b++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+    
+    return 0;
+}
