14f2aeed0f44afcbc69560740be591e20732e4f9
aakki3521-afk <aakki3521@gmail.com>
2026-01-05 17:44:26 +0000

    Add files via upload
    count odd in an array

---
+++ count odd array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int arr[5];
+int n=3;
+int count=0;
+printf("enter the num");
+scanf("%d",&arr[5]);
+for( int i=0;i<n;i++){
+
+    scanf("%d",&arr[i]);
+}
+for(int i=0;i<n;i++){
+    if(arr[i]%2!=0){
+        count++;
+    }
+}
+  printf("total even num are=%d\n",count); 
+    
+
+
+    
+    return 0;
+}
