f06dbc6c5b02d991b2e7942afda882a0071dcad6
aakki3521-afk <aakki3521@gmail.com>
2026-01-05 17:45:14 +0000

    Add files via upload
    count positive num in an array

---
+++ count positive in array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int arr[5];
+int n=5;
+int count=0;
+printf("enter the num");
+scanf("%d",&arr[5]);
+for( int i=0;i<n;i++){
+
+    scanf("%d",&arr[i]);
+}
+for(int i=0;i<n;i++){
+    if(arr[i]>=0){
+        
+        count++;
+    }
+}
+  printf("total even num are=%d\n",count); 
+    
+
+
+    
+    return 0;
+}
