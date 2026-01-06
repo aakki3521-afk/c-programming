0ef2478e8caceef0d7be4234df929606e601ceac
aakki3521-afk <aakki3521@gmail.com>
2026-01-06 05:11:39 +0000

    Add files via upload
    copy one array to another

---
+++ copy array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int a[50],b[50];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for( int i=0;i<n;i++){
+        b[i]=a[i];
+
+    }for( int i=0;i<n;i++){
+        printf("%d",b[i]);
+    }
+    
+    return 0;
+}
