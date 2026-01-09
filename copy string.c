d5d7b64f0df1df200df2659e0ab3f39c28ee7cb3
aakki3521-afk <aakki3521@gmail.com>
2026-01-09 18:26:37 +0000

    Add files via upload
    copy string

---
+++ copy string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    char s1[20];
+    char s2[20];
+    
+    scanf("%s",s1);
+        strcpy(s2,s1);
+    printf("%s\n",s2);
+
+    return 0;
+}
