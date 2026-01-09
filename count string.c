8649b3a7fc1406accf6de41af2af108e4c4b4064
aakki3521-afk <aakki3521@gmail.com>
2026-01-09 18:19:38 +0000

    Add files via upload
    count vowels in string

---
+++ count string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+char s[20];
+int n,count=0;
+scanf("%d",&n);
+scanf("%s",&s);
+for(int i=0;i<s[i]!='\0';i++)
+
+
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+ count++;
+}
+
+
+printf("%d",count);
+    
+    return 0;
+}
