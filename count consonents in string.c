1c5d00190ed7992262b20be6de757668444f3383
aakki3521-afk <aakki3521@gmail.com>
2026-01-09 18:20:31 +0000

    Add files via upload
    count consonents in c

---
+++ count consonents in string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdio.h>
+int main() {
+char s[20];
+int n,count=0;
+scanf("%d",&n);
+scanf("%s",&s);
+for(int i=0;i<s[i]!='\0';i++)
+
+
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'!=n){
+ 
+}else{
+    count++;
+}
+
+
+printf("%d",count);
+    
+    return 0;
+}
