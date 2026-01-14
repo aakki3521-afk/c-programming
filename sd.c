f777d6f2d57bcef6dc95dc7ced9c757090dc8a07
aakki3521-afk <aakki3521@gmail.com>
2026-01-14 16:41:03 +0000

    Add files via upload
    add 2 num in c

---
+++ sd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+        printf("enter the values");
+       scanf("%d %d",&a,&b);
+    printf("enter the values");
+    c=a/b;
+    printf("c is the valur of%d",c);
+    
+    return 0;
+}
