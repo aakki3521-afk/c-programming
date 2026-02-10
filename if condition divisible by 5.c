2b3739c2b7bd77864a2d1497f88e1a55d17d7037
aakki3521-afk <aakki3521@gmail.com>
2026-02-10 21:03:06 +0530

    Add files via upload
    if condition divisible by 5

---
+++ if condition divisible by 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+      int num;
+      scanf("%d",&num);
+      if(num%5==0){
+        printf("it is divisible by 5");
+
+      }else{
+        printf("not divisivle by num 5");
+      }
+    
+    return 0;
+}
