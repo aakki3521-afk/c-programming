30f16ec5442f1c265fdab01d603698e9ad885fb3
aakki3521-afk <aakki3521@gmail.com>
2026-02-10 20:58:53 +0530

    Add files via upload
    the num is divisible by 11 or not

---
+++ if condition 11 div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+      int num;
+      scanf("%d",&num);
+      if(num%11==0){
+        printf("it is divisible by 11");
+
+      }else{
+        printf("not divisivle by num 11");
+      }
+    
+    return 0;
+}
