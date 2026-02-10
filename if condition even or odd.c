40337943b9f3614ee563d496617f38552859da80
aakki3521-afk <aakki3521@gmail.com>
2026-02-10 21:02:20 +0530

    Add files via upload
    if condition even or odd

---
+++ if condition even or odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("its an even num");
+    }
+    else{
+        printf("its an odd num");
+    }
+    
+    return 0;
+}
