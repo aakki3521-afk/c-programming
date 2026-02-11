599c01ede3185054a9668d70be4c929512e40314
aakki3521-afk <aakki3521@gmail.com>
2026-02-11 10:35:45 +0530

    Add files via upload
    room temperature in switch

---
+++ room temp in switch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int room,season,A,B,C,D,peak;
+    scanf("%d%d",&room,&season);
+    switch(room){
+        case 1:
+        //int standard;
+        if(peak==A){
+            printf("2500");
+        }else{
+            printf("2000");
+        }
+        break;
+        case 2:
+        //int dulexe;
+        if(peak==C){
+            printf("4000");
+        }else{
+            printf("3000");
+        }
+        break;
+        default:
+        printf("the invalid value");
+
+
+    }
+    
+    return 0;
+}
