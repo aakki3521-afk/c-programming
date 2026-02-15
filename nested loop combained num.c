c3e7c086a98d6936278e6fbe398ed0096701152d
aakki3521-afk <aakki3521@gmail.com>
2026-02-15 22:00:24 +0530

    Add files via upload
    nested loop

---
+++ nested loop combained num.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){//i=1
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+    
+        for(int k=1;k<(n-i)*2;k++){
+            printf(" ");
+        }
+            
+
+        for(int j=i;j>=1;j--){
+                printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
