69c0e247ebd28b6571d45142761e14edfc18936f
aakki3521-afk <aakki3521@gmail.com>
2026-02-19 20:19:28 +0530

    Add files via upload
    sal program in loop

---
+++ sal program in loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n,day,sal;
+    scanf("%d %d",&n,&day);
+    for(int i=0;i<n;i++){
+        sal=n-(day*100);
+    }printf("%d",sal);
+    
+    return 0;
+}
