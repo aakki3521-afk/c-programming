f4afe9899cd4b31da44c86d7284c82ac42cc572c
aakki3521-afk <aakki3521@gmail.com>
2026-02-22 22:06:12 +0530

    Add files via upload
    polindrome

--- polindrame.c
+++ polindrame.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main() {
-    int a,sum=0,on,r;
+    int a,sum=0,r;
     printf("enter the num");
     scanf("%d",&a);
-    on=a;
+    //on=a;
     while(a!=0){
         r=a%10;
         sum=sum*10+r;
         a=a/10;
     }
 printf("r is equal to %d\n",sum);
-if (on=sum){
+if (a=sum){
     printf("its a polindrome\n");
 }
 else {
