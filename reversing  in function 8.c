dcc200721644645ff71a0ea50ab24fa00e45efc4
aakki3521-afk <aakki3521@gmail.com>
2026-02-28 16:28:07 +0530

    Add files via upload
    reversing the function

---
+++ reversing  in function 8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int sum(int n){
+    int tol=0;
+    while(n!=0){
+     int val=n%10;//
+     tol=tol*10+val;//
+     n=n/10;
+     }
+
+
+    
+    return tol;
+}
+int main() {
+    int a;
+    printf("enter the num=");
+    scanf("%d",&a);
+    int x;
+    x=sum(a);
+    printf("%d",x);
+
+    
+    
+    return 0;
+}
