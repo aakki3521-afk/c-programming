c5e3e5191d97302f908b9148dbe8d08378a1787a
aakki3521-afk <aakki3521@gmail.com>
2026-03-15 20:17:02 +0530

    Add files via upload
    2d array

--- 2d array 1.c
+++ 2d array 1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int r,c,max=0;
+    int r,c,count=0,i,j;
     scanf("%d%d",&r,&c);
     int a[r][c];
     for(int i=0;i<r;i++){
@@ -8,14 +8,23 @@ int main() {
     scanf("%d",&a[i][j]);
         }
     }
+    int temp;
+    //scanf("%d",&temp);
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
-            if(max<a[i][j]){
-                max=a[i][j];
+            if(a[i][j]=temp){
+                temp=a[i][j];
+                count++;
             }
         }
-    }
-    printf("%d",max);printf("\n");
+    }    printf("%d\n",count);
+
+    
+        
+    //printf("%d\n",max);
+
+
+    printf("\n");
     
     
     return 0;
