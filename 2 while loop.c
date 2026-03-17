6f453925169df7125aa1f2bc116a32143594337a
aakki3521-afk <aakki3521@gmail.com>
2026-03-17 21:01:11 +0530

    Add files via upload
    while loop 2

---
+++ 2 while loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n,m,c,t;
+    int low=0;
+    //printf("ENTER THE VALUES N= ");
+    scanf("%d%d",&n,&m);
+    c==m;
+    while(n!=0){
+        //printf("ENTER THE INITIAL BALANCE");
+        scanf("%d",&t);
+        m=m+t;
+        if(c<0){
+            low++;
+        }
+    }printf("%d",c);
+    printf("%d",low);
+
+    
+    return 0;
+}
