56a1b0ba66a3eec9275aa1531d0b684a586ed261
aakki3521-afk <aakki3521@gmail.com>
2026-04-10 21:07:54 +0530

    Add files via upload
    while

---
+++ data usage in the while loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n,count=0;
+    scanf("%d",&n);
+    int data;
+    while(n>=1){
+        scanf("%d",&data);
+        if(data>=1){
+            count++;
+        }
+    }n--;
+    printf("%d",count);
+        
+    
+    
+    return 0;
+}
