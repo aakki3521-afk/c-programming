be91ab196c4755c4d11469303ac6e6acf960f890
aakki3521-afk <aakki3521@gmail.com>
2026-05-20 19:30:58 +0530

    Add files via upload
    about datatypes

---
+++ data types.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    printf("data types:         \n");
+    printf("Int:it is used for numbers\n");
+    int a=5;
+    printf("a=%d\n:",a);
+    int nbc=10;
+    printf("nbc=%d\n",nbc);
+    printf("FLOAT:it is used for decimal\n");
+    float p=0.6;
+    printf("%f\n",p);
+    float u=9876;
+    printf("%f\n",u);
+    printf("CHAR=IT IS USED FOR LRTTERS\n");
+    char ch='a';
+    printf("ch:%c\n",ch);
+    char m='2';
+    printf("m:%c\n",m);
+    printf("CHAR ARRAY:COLLECTION OF CHARACTERS\n");
+    char h[]="123445";
+         printf("123456677:%s\n",h);
+         char f[]="asdfghh(*&)";
+         printf("asdfghjjk:%s\n",f);
+    
+    
+    return 0;
+}
